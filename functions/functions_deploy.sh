c171dda71d975c8959e0b661297761b4b90cadaf
Kurucz Gábor <48492301+Qrcy@users.noreply.github.com>
2019-04-21 15:52:49 +0200

    scale

--- functions/functions_deploy.sh
+++ functions/functions_deploy.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-
 ## Java környzet
-fission env create --name nodejs --image fission/node-env
+fission environment create --name nodejs --image fission/node-env --mincpu 40 --maxcpu 80 --minmemory 64 --maxmemory 128 --poolsize 4
 
 ## Go környzet
-fission environment create --name go --image fission/go-env --builder fission/go-builder
+fission environment create --name go --image fission/go-env --builder fission/go-builder --mincpu 40 --maxcpu 80 --minmemory 64 --maxmemory 128 --poolsize 4
 
 ## Python környezet
-fission environment create --name python --image fission/python-env 
+fission environment create --name python --image fission/python-env --mincpu 40 --maxcpu 80 --minmemory 64 --maxmemory 128 --poolsize 4
 
 ## Java fügvények
-fission function create --name hellojava --code ./deploys/js/hellojava.js --env nodejs
-fission function create --name primjava --code ./deploys/js/primjava.js --env nodejs
+fission function create --name hellojava --code ./deploys/js/hellojava.js --env nodejs --minscale 1 --maxscale 5  --executortype newdeploy
+fission function create --name primjava --code ./deploys/js/primjava.js --env nodejs --minscale 1 --maxscale 5  --executortype newdeploy
 
 ## Go függvények
-fission function create --name hellogo --env go --src ./deploys/go/hellogo.go --entrypoint Handler
+fission function create --name hellogo --src ./deploys/go/hellogo.go --entrypoint Handler --env go --minscale 1 --maxscale 5  --executortype newdeploy
 
 ## Python fügvények
-fission function create --name hellopython --env python --code ./deploys/hellopython.py
+fission function create --name hellopython --code ./deploys/hellopython.py --env python --minscale 1 --maxscale 5  --executortype newdeploy
 
 ## HTTP Triggerek
 fission httptrigger create --url /hellojava --method GET --function hellojava
@@ -26,5 +25,5 @@ fission httptrigger create --url /hellogo --method GET --function hellogo
 fission httptrigger create --url /hellopython --method GET --function hellopython
 
 fission httptrigger create --url /primjava --method POST --function primjava
-##fission httptrigger create --url /hellogo --method GET --function primgo
-##fission httptrigger create --url /hellopython --method GET --function primpython
+##fission httptrigger create --url /hellogo --method POST --function primgo
+##fission httptrigger create --url /hellopython --method POST --function primpython
