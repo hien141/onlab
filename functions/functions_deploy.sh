fcc0a3b0e861b061a79659b33d5a4bc3467cda6c
Kurucz Gábor <48492301+Qrcy@users.noreply.github.com>
2019-04-24 20:03:46 +0200

    typo

--- functions/functions_deploy.sh
+++ functions/functions_deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "4 min wait time to pods start"
+sleep 4m
+
 ## Java környzet
 fission environment create --name nodejs --image fission/node-env --mincpu 40 --maxcpu 80 --minmemory 64 --maxmemory 128 --poolsize 4
 
@@ -10,14 +13,14 @@ fission environment create --name go --image fission/go-env --builder fission/go
 fission environment create --name python --image fission/python-env --mincpu 40 --maxcpu 80 --minmemory 64 --maxmemory 128 --poolsize 4
 
 ## Java fügvények
-fission function create --name hellojava --code ./deploys/js/hellojava.js --env nodejs --minscale 1 --maxscale 5  --executortype newdeploy
-fission function create --name primjava --code ./deploys/js/primjava.js --env nodejs --minscale 1 --maxscale 5  --executortype newdeploy
+fission function create --name hellojava --code ./functions/js/hellojava.js --env nodejs --minscale 1 --maxscale 5  --executortype newdeploy
+fission function create --name primjava --code ./functions/js/primjava.js --env nodejs --minscale 1 --maxscale 5  --executortype newdeploy
 
 ## Go függvények
-fission function create --name hellogo --src ./deploys/go/hellogo.go --entrypoint Handler --env go --minscale 1 --maxscale 5  --executortype newdeploy
+fission function create --name hellogo --src ./functions/go/hellogo.go --entrypoint Handler --env go --minscale 1 --maxscale 5  --executortype newdeploy
 
 ## Python fügvények
-fission function create --name hellopython --code ./deploys/hellopython.py --env python --minscale 1 --maxscale 5  --executortype newdeploy
+fission function create --name hellopython --code ./functions/python/hellopython.py --env python --minscale 1 --maxscale 5  --executortype newdeploy
 
 ## HTTP Triggerek
 fission httptrigger create --url /hellojava --method GET --function hellojava
